b4c349851fbd695e05ed406d144fd05e7198849a
Matias <matiascharrut@gmail.com>
2020-06-05 18:37:51 -0300

    Mejorado parcialmenteshunting_yard

--- shunting_yard.cpp
+++ shunting_yard.cpp
@@ -1,36 +1,102 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include "lista.h"
 #include "shunting_yard.h"
 
 
 //ATENCION, ESTE CODIGO DEBE PERFECCIONARSE INTERNAMENTE (MATI NO CUELGUES, FIRMA MATI)
+//FALTA RETOCAR PRECEDENCIA
+//RETOCAR LO DE LOS PARENTESIS Y LAS STRING
+//VER SI HAY MEJOR MANERA DE CHEQUEAR QUE NO ()
 using namespace std;
+
+int main(int argc, char ** argv){
+	if(argc < 2)
+		return 0;
+	cout << shuntingYard(argv[1]) << endl;
+	return 0;
+}
+
 
 lista<string> shuntingYard(string input){
 	lista<string> cola_salida;
 	lista<string> pila_operadores;
-	string operador;
+	string extraido;
+	
+	//Por ahora defino los vectores aca hasta que nos entreguen el tp0 para ver si hay que cambiar algo.
+	static string operadores[] = {"+", "-","*", "/", "^"};
+	static size_t operadores_size = 5;
+	static string funciones[] = {"re", "im", "abs", "phase", "exp", "ln", "sin", "cos"};
+	static size_t funciones_size = 8;
+	static string caracteresEspecial[] = {"z", "j"};
+	static size_t caracteresEspecial_size = 2;
+	
+	bool flagNumero = false;
+	bool flagOperador = false;
+	bool flagFuncion = false;
+	bool flagParentesis = false;
+	
+	
 	while(!(input = quitarEspaciosInicio(input)).empty()){
-		if(isdigit(input[0]))
-			cola_salida.enqueue(leerNumero(input));
-		else if(input[0] == 'z'){
-			cola_salida.enqueue("z"); //muy harcodeado
-			input = input.substr(1); //sacar estos substr creo O HACER TODO DESDE ACA (VER) //RETOCAR ESTO Y QUE QUEDE TODO EN UN MISMO IF
+		if(isdigit(input[0])){
+			if(flagNumero){
+				cout << MSJ_ERROR_NUMEROS_SEG << endl;
+				exit(1);
+			}
+			flagOperador = false;
+			flagFuncion = false;
+			flagParentesis = false;
+			flagNumero = true;
+			extraido = leerNumero(input);
+			cola_salida.enqueue(extraido);
+			input = input.substr(extraido.length());
+			
 		}
-		else if(input[0] == 'j'){
-			cola_salida.enqueue("j"); //muy harcodeado
-			input = input.substr(1); //sacar estos substr creo O HACER TODO DESDE ACA (VER)
+			
+		else if(!(extraido = leerToken(input, caracteresEspecial, caracteresEspecial_size)).empty()){
+			if(flagNumero){
+				cout << MSJ_ERROR_NUMEROS_SEG << endl;
+				exit(1);
+			}
+			flagOperador = false;
+			flagFuncion = false;
+			flagNumero = true;
+			flagParentesis = false;
+			pila_operadores.push(extraido);
+			input = input.substr(extraido.length());
 		}
-		else if(!(operador = leerFuncion(input)).empty())
-			pila_operadores.push(operador);
-		else if(!(operador = leerOperador(input)).empty()){
+		else if(!(extraido = leerToken(input, funciones, funciones_size)).empty()){
+			if(flagNumero){
+				cout << MSJ_ERROR_NUMERO_FUNC_SEG << endl;
+				exit(1);
+			}
+			flagFuncion = true;
+			flagOperador = false;
+			flagNumero = false;
+			flagParentesis = false;
+			pila_operadores.push(extraido);
+			input = input.substr(extraido.length());
+		}
+		
+		else if(!(extraido = leerToken(input, operadores, operadores_size)).empty()){
+			if(flagOperador){
+				cout << MSJ_ERROR_OPERADORES_SEG << endl;
+				exit(1);
+			}
+			if(flagFuncion){
+				cout << MSJ_ERROR_FUNC_OPERADOR_SEG << endl;
+				exit(1);
+			}
+			flagOperador = true;
+			flagNumero = false;
+			flagParentesis = false;
 			while(pila_operadores.llena()){
 				const string operador_top = pila_operadores.mirarTop();
 				//if(!esOperador(operador_top)) //LO BORRO CREO QUE NO HACE FALTA QUE SEA OPERADOR
 				//	break;
-				if(precedencia(operador_top) <= precedencia(operador)){
-					if((precedencia(operador_top) < precedencia(operador)) || !izqAsoc(operador)) //aclara que aca aunque izqAsoc no anda bien igual funciona
+				if(precedencia(operador_top) <= precedencia(extraido)){
+					if((precedencia(operador_top) < precedencia(extraido)) || !izqAsoc(extraido)) //aclara que aca aunque izqAsoc no anda bien igual funciona
 						break;
 				}
 				if(operador_top == "(")
@@ -38,17 +104,25 @@ lista<string> shuntingYard(string input){
 				
 				cola_salida.enqueue(pila_operadores.pop());
 			}
-			pila_operadores.push(operador);
+			pila_operadores.push(extraido);
+			input = input.substr(extraido.length());
 		}
-		else if(input[0]=='('){
+		
+		else if(input[0] == '('){
+			flagParentesis = true;
 			string operador_insertar(1, input[0]);
 			pila_operadores.push(operador_insertar);
 			input = input.substr(1);
 		}
-		else if(input[0]==')'){
+		else if(input[0] == ')'){
+			if(flagParentesis){
+				cout << MSJ_ERROR_PARENTESIS_VACIOS << endl;
+				exit(1);
+			}
+			flagParentesis = false;
 			while(1){
 				if(pila_operadores.vacia()){
-					cout << "Error" << endl;
+					cout << MSJ_ERROR_PARENTESIS << endl;
 					exit(1);
 				}
 				const string operador_top = pila_operadores.mirarTop();
@@ -61,17 +135,27 @@ lista<string> shuntingYard(string input){
 			input = input.substr(1);
 		}
 		else{
-			cout << "Error que termina" << endl;
+			cout << MSJ_ERROR_OPERADOR_DESC << endl;
 			exit(1);
 		}
 	} //fin del while
+	
+	if(flagOperador){
+		cout << MSJ_ERROR_OPERADOR_FINAL << endl;
+		exit(1);
+	}
+	if(flagFuncion){
+		cout << MSJ_ERROR_FUNCION_FINAL << endl;
+		exit(1);
+	}
+	
 	while(pila_operadores.llena()){
-		operador = pila_operadores.pop();
-		if(operador == "("){
-			cout << "Error" << endl;
+		const string operador_top = pila_operadores.pop();
+		if(operador_top == "("){
+			cout << MSJ_ERROR_PARENTESIS << endl;
 			exit(1);
 		}
-		cola_salida.enqueue(operador);
+		cola_salida.enqueue(operador_top);
 	}
 	return cola_salida;
 }
@@ -83,27 +167,14 @@ bool izqAsoc(string input){
 	return true;
 }
 
-string leerOperador(string& input){
-	char operadores [] = {'+', '-','*', '/', '^'};
-	for(int i = 0; i < 5; i++){
-		if(input[0] == operadores[i]){
-			input = input.substr(1);
-			string operador(1, operadores[i]);
-			return operador;
-		}
-	}
-	return "";
-}
 
-string leerFuncion(string& input){
-	string funcion [] = {"re", "im", "abs", "phase", "exp", "ln"};
-	for(int i = 0; i < 6; i++){
-		if(input.substr(0, funcion[i].length()) == funcion[i]){
-			input = input.substr(funcion[i].length());
-			return funcion[i];
+string leerToken(string input, string* vector, size_t vector_size){
+	for(size_t i = 0; i < vector_size; i++){
+		if(!input.compare(0, vector[i].length(), vector[i])){
+			return vector[i];
 		}
-	}
-	return "";
+	} 
+	return EMPTY_STRING;
 }
 
 int precedencia(string operador){
@@ -116,14 +187,13 @@ int precedencia(string operador){
 }
 
 
-string leerNumero(string& input){
+string leerNumero(string input){
 	string numero = "";
 	int i = 0;
-	while(isdigit(input[i]) || input[i] == '.'){
+	while(isdigit(input[i]) || input[i] == DECIMAL_CHAR){
 		numero += input[i];
 		i++;
 	}
-	input = input.substr(i);
 	return numero;
 }
 
--- shunting_yard.h
+++ shunting_yard.h
@@ -1,13 +1,26 @@
 #ifndef SHUNTING_YARD__H
 #define SHUNTING_YARD__H
 
+#define EMPTY_STRING ""
+#define DECIMAL_CHAR '.'
+#define MSJ_ERROR_PARENTESIS "Error: parentesis no balanceados"
+#define MSJ_ERROR_OPERADOR_DESC "Error: funcion o operador desconocido"
+#define LEFT_PARENTESIS_OP "("
+#define RIGHT_PARENTESIS_OP ")"
+#define MSJ_ERROR_NUMEROS_SEG "Error: dos numeros seguidos"
+#define MSJ_ERROR_OPERADORES_SEG "Error: dos operadores/funciones seguidos"
+#define MSJ_ERROR_OPERADOR_FINAL "Error: la entrada termina con un operador"
+#define MSJ_ERROR_FUNC_OPERADOR_SEG "Error: una funcion no puede ir seguida de un operador"
+#define MSJ_ERROR_FUNCION_FINAL "Error: la entrada termina con una funcion"
+#define MSJ_ERROR_NUMERO_FUNC_SEG "Error: un numero no puede estar seguido de una funcion"
+#define MSJ_ERROR_PARENTESIS_VACIOS "Error: parentesis vacios"
+
 
 lista<string> shuntingYard(string input);
 bool izqAsoc(string);
-string leerOperador(string&);
-string leerFuncion(string&);
+string leerToken(string, string*, size_t);
 int precedencia(string);
-string leerNumero(string&);
+string leerNumero(string);
 string quitarEspaciosInicio(string); //ver que hacer con esta
 
 
